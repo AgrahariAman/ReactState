d95d110779c407873688108a23665d9631e67542
Agrahari <aman.agrahari@accenture.com>
2021-12-15 14:25:47 +0530

    cart item

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -11,6 +11,32 @@ import Detail from "./Detail";
 import Cart from "./Cart";
 
 export default function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(id, sku) {
+    setCart((items) => {
+      const itemInCart = items.find((i) => i.sku === sku);
+      if (itemInCart) {
+        //return a new array with the matching item replaced
+        return items.map((i) =>
+          i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      } else {
+        // Return new array with the new item appended
+        return [...items, { id, sku, quantity: 1 }];
+      }
+    });
+  }
+
+  function updateQuantity(sku, quantity) {
+    setCart((item) => {
+      if (quantity === 0) {
+        return item.filter((i) => i.sku !== sku);
+      }
+      return item.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+    });
+  }
+
   return (
     <>
       <div className="content">
@@ -19,8 +45,14 @@ export default function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to Fitness Shoes</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route path="/:category/:id" element={<Detail />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/:category/:id"
+              element={<Detail addToCart={addToCart} />}
+            />
+            <Route
+              path="/cart"
+              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+            />
           </Routes>
         </main>
       </div>
